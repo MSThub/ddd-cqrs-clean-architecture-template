a5afc1f2df95897dc16e6e0e5c2be55fe9342709
MST <MSTMadridista@gmail.com>
2026-01-01 23:51:10 +0330

    chore: add unit test scaffolding to the template

---
+++ Tests/AT.UnitTests/Records/Commands/CreateRecordCommandHandlerTests.cs
@@ -0,0 +1,15 @@
+﻿using AT.Application.Abstractions.Handlers;
+using AT.Application.Records.Commands.CreateRecord;
+using AT.TestTools.Records.Builders;
+
+namespace AT.UnitTests.Records.Commands;
+
+public class CreateRecordCommandHandlerTests
+{
+    private readonly ICommandHandler<CreateRecordCommand, string> _sut;
+
+    public CreateRecordCommandHandlerTests()
+    {
+        _sut = new CreateRecordCommandHandlerBuilder().Build();
+    }
+}
---
+++ Tests/AT.UnitTests/Records/Queries/GetRecordByIdQueryHandlerTests.cs
@@ -0,0 +1,14 @@
+﻿using AT.Application.Records.Queries.GetRecordById;
+using AT.TestTools.Records.Builders;
+
+namespace AT.UnitTests.Records.Queries;
+
+public class GetRecordByIdQueryHandlerTests
+{
+    private readonly GetRecordByIdQueryHandler _sut;
+
+    public GetRecordByIdQueryHandlerTests()
+    {
+        _sut = new GetRecordByIdQueryHandlerBuilder().Build();
+    }
+}
